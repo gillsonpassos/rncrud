50af544c3044d5f290f6753fe1366d75451a5c9a
gillsonpassos <oliveirapassos07@outlook.com>
2024-02-02 09:19:14 -0300

    Lista de Usuário #01

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,35 @@
-import React from "react";
-import { Text } from "react-native";
+import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import Users from '../data/Users'
+import { ListItem, Avatar } from 'react-native-elements'
 
 export default props => {
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem
+                onPress={() => props.navigation.navigate('UserForm')}>
+                <Avatar
+                    title={user.name}
+                    Subtitle={user.email}
+                    source={{ uri: user.avatarUrl }}
+                    bottomDivider
+                    onPress={() => props.navigation.navigate('UserForm')}
+                />
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
     return (
-        <Text>UserList</Text>
+        <View>
+            <FlatList
+                keyExtractor={user => user.id.toString()}
+                data={Users}
+                renderItem={getUserItem}
+            />
+        </View>
     )
 }
