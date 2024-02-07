5f29e96afd2e63f479c5fbc3e2ff3038efd5d12f
gillsonpassos <oliveirapassos07@outlook.com>
2024-02-07 12:06:33 -0300

    Use Reducer #03

--- src/context/UsersContext.js
+++ src/context/UsersContext.js
@@ -1,21 +1,33 @@
-import React, { createContext, useReducer } from "react";
-import Users from "../data/Users";
+import React, { createContext, useReducer } from 'react'
+import Users from '../data/Users'
 
-const initialState = { Users }
-
+const initialState = { Users };
 const UsersContext = createContext({})
 
 const actions = {
+    createUser(state, action) {
+        const user = action.payload
+        user.id = Math.random()
+        return {
+            ...state,
+            Users: [...state.Users, user],
+        }
+    },
+    updateUser(state, action) {
+        const updated = action.payload
+        return {
+            ...state,
+            Users: state.Users.map(u => u.id === updated.id ? updated : u)
+        }
+    },
     deleteUser(state, action) {
         const user = action.payload
         return {
-            // ...state,
+            ...state,
             Users: state.Users.filter(u => u.id !== user.id)
         }
     }
-
 }
-
 
 export const UsersProvider = props => {
 
@@ -23,7 +35,6 @@ export const UsersProvider = props => {
         const fn = actions[action.type]
         return fn ? fn(state, action) : state
     }
-
 
 
     const [state, dispatch] = useReducer(reducer, initialState)
@@ -35,6 +46,6 @@ export const UsersProvider = props => {
     )
 }
 
-
-
 export default UsersContext
+
+
--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
-import { Text, View, TextInput, StyleSheet, Button } from "react-native";
+import React, { useState, useContext } from 'react'
+import { Text, View, TextInput, StyleSheet, Button } from 'react-native'
+import UsersContext from '../context/UsersContext'
 
 export default ({ route, navigation }) => {
     const [user, setUser] = useState(route.params ? route.params : {})
+    const { dispatch } = useContext(UsersContext)
+
     return (
         <View style={style.form}>
             <Text>Nome</Text>
@@ -12,28 +15,31 @@ export default ({ route, navigation }) => {
                 placeholder="Informe o Nome"
                 value={user.name}
             />
-            <Text>E-mail</Text>
+            <Text>Email</Text>
             <TextInput
                 style={style.input}
                 onChangeText={email => setUser({ ...user, email })}
                 placeholder="Informe o E-mail"
                 value={user.email}
             />
-            <Text>Url do Avatar</Text>
+            <Text>URL do Avatar</Text>
             <TextInput
                 style={style.input}
                 onChangeText={avatarUrl => setUser({ ...user, avatarUrl })}
-                placeholder="Informe a Url do Avatar"
+                placeholder="Informe a URL do Avatar"
                 value={user.avatarUrl}
             />
             <Button
-                title='Salvar'
+                title="Salvar"
                 onPress={() => {
+                    dispatch({
+                        type: user.id ? 'updateUser' : 'createUser',
+                        payload: user,
+                    })
                     navigation.goBack()
                 }}
             />
         </View>
-
     )
 }
 
@@ -45,6 +51,6 @@ const style = StyleSheet.create({
         height: 40,
         borderColor: 'gray',
         borderWidth: 1,
-        marginBottom: 10,
+        marginBottom: 15,
     }
 })
